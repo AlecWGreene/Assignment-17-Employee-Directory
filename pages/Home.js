d8eeeb9d8b7424464ed968c3761855d8ed57b6f0
Alec W Greene <alecwgreene@gmail.com>
2020-10-29 10:50:55 -0400

    Updated home page text

--- pages/Home.js
+++ pages/Home.js
@@ -1,10 +1,23 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 export default function Home(){
     return (
         <View>
-            <Text>Home page placeholder</Text>
+            <Text style={styles.header}>Welcome to the Employee Directory</Text>
+            <Text style={styles.bodyText}>This app was created using react native and some pre-generated data, cycle through the pages and click on the headers to view/sort the data.</Text>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    header: {
+        fontWeight: "bold",
+        fontSize: "24pt",
+        textAlign: "center"
+    },
+    bodyText: {
+        fontSize: "18pt",
+        textAlign: "center"
+    }
+});
